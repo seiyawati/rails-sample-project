eff68f92861d115223a0b35bd39d4e4352cdf9a8
Higurashi123 <61195154+Higurashi123@users.noreply.github.com>
2022-01-22 23:56:38 +0900

    会員ログイン機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+   # 現状使っていない 2022/01/22
+    def logged_in_user
+      redirect_to login_url unless logged_in?
+    end
 end
